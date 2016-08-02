d254bd46704df39a3ec4facee29af81c3b3a48e0
Luke Ni <ning.ni@commscope.com>
2016-08-02 17:20:04 +0800

    update

--- stock1.R
+++ stock1.R
@@ -64,7 +64,7 @@ two.week.breakup <- function(x)
       week1max <- max(x[week1start:week2start])
       week2max <- max(x[week2start:i])
       
-      if(week2max <= week1max)
+      if(week2max <= week1max * 1.005)
         breakup[i] = FALSE
       else
         breakup[i] = TRUE
@@ -76,23 +76,24 @@ two.week.breakup <- function(x)
 
 a <- function(x)
 {
-  return (subset(x, select=c(Close,PE, PB, Returns, ClosePercentile, PEPercentile, VolumnPercentile, AmplitudePercentile, Breakdown, Breakup)))
+  return (subset(x, select=c(Score, Close, NextReturns, PE, PB, Returns, ClosePercentile, PEPercentile, VolumnPercentile, MomentumPercentile, AmplitudePercentile, Breakdown, Breakup)))
 }
 
-load.data.df <- function(file)
-{
-  data <- read.table(file, header= TRUE, sep=",", stringsAsFactors = FALSE, colClasses=c("factor", "NULL", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "NULL", "NULL", "NULL", "NULL", "NULL", "numeric" ), col.names=c("Code", "Name", "Date", "Open", "High", "Low", "Close", "Volumn", "Amount", "Change", "Turnover", "PE", "PB", "Average", "AmountPercentage", "HQLTSZ", "AHQLTSZ", "Payout", "IR"))
-  data <- subset(data, select=c(Date, Open, Close, High, Low, Volumn, PE, PB))
-  returns <- -round(c(diff(data$Close), NA) / data$Close, 4) * 100
-  reversed.Close = rev(data$Close)
-  reversed.PE = rev(data$PE)
-  reversed.Low = rev(data$Low)
-  reversed.High = rev(data$High)
-  reversed.Volumn = rev(data$Volumn)
-  reversed.Amplitude = rev((data$High - data$Low) / data$Close)
-  y <- year(data$Date)
-  m <- month(data$Date)
-  wd <- wday(data$Date) - 1 
+load.data.df <- function(file) { data <- read.table(file, header= TRUE, sep=",", stringsAsFactors = FALSE, 
+  colClasses=c("factor", "NULL", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "NULL", "NULL", "NULL", "NULL", "NULL", "numeric" ), 
+  col.names=c("Code", "Name", "Date", "Open", "High", "Low", "Close", "Volumn", "Amount", "Change", "Turnover", "PE", "PB", "Average", "AmountPercentage", "HQLTSZ", "AHQLTSZ", "Payout", "IR")) 
+  data <- subset(data, select=c(Date, Open, Close, High, Low, Volumn, PE, PB, IR)) 
+  returns <- -round(c(diff(data$Close), NA) / data$Close, 4) * 100 
+  reversed.Close = rev(data$Close) 
+  reversed.PE = rev(data$PE) 
+  reversed.Low = rev(data$Low) 
+  reversed.High = rev(data$High) 
+  reversed.Volumn = rev(data$Volumn) 
+  reversed.Amplitude = rev((data$High - data$Low) / data$Close) 
+  reversed.Momentum = rev(returns * data$Volumn) 
+  y <- year(data$Date) 
+  m <- month(data$Date) 
+  wd <- wday(data$Date) - 1  
   md <- mday(data$Date)
   ma10 <- SMA(reversed.Close, 10)
   ma20 <- SMA(reversed.Close, 20)
@@ -101,11 +102,37 @@ load.data.df <- function(file)
   pe.percentile <- one.year.percenctile(reversed.PE)
   volumn.percentile <- one.year.percenctile(reversed.Volumn)
   amplitude.percentile <- one.year.percenctile(reversed.Amplitude)
-  two.week.breakdown <- two.week.breakdown(reversed.Low)
-  two.week.breakup <- two.week.breakup(reversed.High)
-  data <- cbind(data, Returns = returns, Year = y, Month = m, Week = wd, Day = md, MA10 = rev(ma10), MA20 = rev(ma20), MA30 = rev(ma30))
-  data <- cbind(data, PrevClose = lead(data$Close), PrevMA10 = lead(data$MA10), PrevMA20 = lead(data$MA20), PrevMA30 = lead(data$MA30))
-  data <- cbind(data, ClosePercentile = rev(close.percentile), PEPercentile = rev(pe.percentile), VolumnPercentile = rev(volumn.percentile), AmplitudePercentile = rev(amplitude.percentile), Breakdown = rev(two.week.breakdown), Breakup = rev(two.week.breakup))
+  momentum.percentile <- one.year.percenctile(reversed.Momentum)
+  two.week.breakdown <- two.week.breakdown(reversed.Close)
+  two.week.breakup <- two.week.breakup(reversed.Close)
+  data <- transform(data, 
+                    Returns = returns,
+                    Year = y,
+                    Month = m,
+                    Week = wd,
+                    Day = md,
+                    MA10 = rev(ma10),
+                    MA20 = rev(ma20),
+                    MA30 = rev(ma30),
+                    PrevClose = lead(data$Close),
+                    NextReturns = (lag(Close, 5) - Close) * 100 / Close,
+                    ClosePercentile = rev(close.percentile),
+                    PEPercentile = rev(pe.percentile),
+                    VolumnPercentile = rev(volumn.percentile),
+                    MomentumPercentile = rev(momentum.percentile),
+                    AmplitudePercentile = rev(amplitude.percentile),
+                    Breakdown = rev(two.week.breakdown),
+                    Breakup = rev(two.week.breakup))
+  score <- (1 - sqrt(data$PEPercentile)) * 5
+  score <- ifelse(data$MA10 > data$MA20, score + 3, score - 3)
+  score <- ifelse(data$Close > data$MA10, score + 4, score - 4)
+  score <- ifelse(data$Breakdown == TRUE, score - 2, score)
+  score <- ifelse(data$Breakup == TRUE, score + 1, score)
+  data <- transform(data, 
+                    PrevMA10 = lead(MA10),
+                    PrevMA20 = lead(MA20),
+                    PrevMA30 = lead(MA30),
+                    Score = score)
   rownames(data) <- data[[1]]
   data$Date <- NULL
   
@@ -158,42 +185,45 @@ todayPEPercentile <- last(stocks$PEPercentile)
 
 returns <- round(diff(stocks$Close) / stocks$Close[1:length(stocks$Close)], 6)
 
-par(mfrow = c(5, 2))
-boxplot(Returns ~ Month, data=monthly.returns, na.rm = TRUE, xlab = "Month")
-grid()
-boxplot(Returns ~ Week, data=stocks.whole, na.rm = TRUE, xlab = "Week")
-grid()
-plot(stocks$Close, type = "l", ylab = "Close", main = paste("current=", todayClose, " Percentile=", todayClosePercentile, sep=""))
-abline(h = quantile(stocks$Close, c(1 - 0.8, 1-0.4, 1-0.2), names = FALSE))
-plot(stocks$PE, type = "l", ylab = "PE", main = paste("current=", todayPE, " Percentile=", todayPEPercentile, sep=""))
-abline(h = quantile(stocks$PE, c(1 - 0.8, 1-0.4, 1-0.2), names = FALSE))
+ par(mfrow = c(5, 2))
+ boxplot(Returns ~ Month, data=monthly.returns, na.rm = TRUE, xlab = "Month")
+ grid()
+ boxplot(Returns ~ Week, data=stocks.whole, na.rm = TRUE, xlab = "Week")
+ grid()
+ plot(stocks$Close, type = "l", ylab = "Close", main = paste("current=", todayClose, " Percentile=", todayClosePercentile, sep=""))
+ abline(h = quantile(stocks$Close, c(1 - 0.8, 1-0.4, 1-0.2), names = FALSE))
+ plot(stocks$PE, type = "l", ylab = "PE", main = paste("current=", todayPE, " Percentile=", todayPEPercentile, sep=""))
+ positions <- quantile(stocks$PE, c(1 - 0.8, 1-0.4, 1-0.2), names = FALSE)
+ abline(h = positions)
+ axis(4, at=positions,labels=c(20, 60, 80), las=2, cex.axis=0.7)
 
-plot(density(stocks$Close), lwd = 3)
-abline(v = todayClose)
-plot(density(stocks$PE), lwd = 3)
-abline(v = todayPE)
 
-monthly.returns.table <- table(sign(monthly.returns$Returns), monthly.returns$Month)
-monthly.returns.table.prop <- prop.table(monthly.returns.table, 2)
-plot(monthly.returns.table.prop[nrow(monthly.returns.table.prop),])
-grid()
+ plot(density(stocks$Close), lwd = 3)
+ abline(v = todayClose)
+ plot(density(stocks$PE), lwd = 3)
+ abline(v = todayPE)
 
-week.returns.table <- table(sign(stocks.whole$Returns), stocks.whole$Week)
-week.returns.table.prop <- prop.table(week.returns.table, 2)
-plot(week.returns.table.prop[nrow(week.returns.table.prop),])
-grid()
+ monthly.returns.table <- table(sign(monthly.returns$Returns), monthly.returns$Month)
+ monthly.returns.table.prop <- prop.table(monthly.returns.table, 2)
+ plot(monthly.returns.table.prop[nrow(monthly.returns.table.prop),])
+ grid()
 
-mday.returns.table <- table(sign(stocks.whole$Returns), stocks.whole$Day)
-mday.returns.table.prop <- prop.table(mday.returns.table, 2)
-plot(mday.returns.table.prop[nrow(mday.returns.table.prop),])
-grid()
+ week.returns.table <- table(sign(stocks.whole$Returns), stocks.whole$Week)
+ week.returns.table.prop <- prop.table(week.returns.table, 2)
+ plot(week.returns.table.prop[nrow(week.returns.table.prop),])
+ grid()
 
-plot(table(cut(stocks$PE, pretty(stocks$PE))))
+ mday.returns.table <- table(sign(stocks.whole$Returns), stocks.whole$Day)
+ mday.returns.table.prop <- prop.table(mday.returns.table, 2)
+ plot(mday.returns.table.prop[nrow(mday.returns.table.prop),])
+ grid()
+
+ plot(table(cut(stocks$PE, pretty(stocks$PE))))
 
 par(mfrow = c(1, 1))
 
-down.ma10 <- stocks.whole[which(stocks.whole$Close < stocks.whole$MA10 & stocks.whole$PrevClose >= stocks.whole$PrevMA10),]
-up.ma10 <- stocks.whole[which(stocks.whole$Close >= stocks.whole$MA10 & stocks.whole$PrevClose < stocks.whole$PrevMA10),]
+down.ma10 <- stocks.whole[stocks.whole$Close < stocks.whole$MA10 & stocks.whole$PrevClose >= stocks.whole$PrevMA10]
+up.ma10 <- stocks.whole[stocks.whole$Close >= stocks.whole$MA10 & stocks.whole$PrevClose < stocks.whole$PrevMA10]
 down.up.ma10 <- rbind(down.ma10, up.ma10)
 down.up.ma10.day.diff <- diff(index(down.up.ma10))
 down.up.ma10.close.diff <- diff(down.up.ma10$Close)
@@ -203,8 +233,8 @@ down.up.ma10.close.diff <- down.up.ma10.close.diff[steps]
 down.up.ma10.day.table <- prop.table(table(down.up.ma10.day.diff))
 down.up.ma10.close.table <- prop.table(table(down.up.ma10.close.diff))
 
-down.ma20 <- stocks.whole[which(stocks.whole$Close < stocks.whole$MA20 & stocks.whole$PrevClose >= stocks.whole$PrevMA20),]
-up.ma20 <- stocks.whole[which(stocks.whole$Close >= stocks.whole$MA20 & stocks.whole$PrevClose < stocks.whole$PrevMA20),]
+down.ma20 <- stocks.whole[stocks.whole$Close < stocks.whole$MA20 & stocks.whole$PrevClose >= stocks.whole$PrevMA20]
+up.ma20 <- stocks.whole[stocks.whole$Close >= stocks.whole$MA20 & stocks.whole$PrevClose < stocks.whole$PrevMA20]
 down.up.ma20 <- rbind(down.ma20, up.ma20)
 down.up.ma20.day.diff <- diff(index(down.up.ma20))
 down.up.ma20.close.diff <- diff(down.up.ma20$Close)
@@ -214,8 +244,8 @@ down.up.ma20.close.diff <- down.up.ma20.close.diff[steps]
 down.up.ma20.day.table <- prop.table(table(down.up.ma20.day.diff))
 down.up.ma20.close.table <- prop.table(table(down.up.ma20.close.diff))
 
-down.ma30 <- stocks.whole[which(stocks.whole$Close < stocks.whole$MA30 & stocks.whole$PrevClose >= stocks.whole$PrevMA30),]
-up.ma30 <- stocks.whole[which(stocks.whole$Close >= stocks.whole$MA30 & stocks.whole$PrevClose < stocks.whole$PrevMA30),]
+down.ma30 <- stocks.whole[stocks.whole$Close < stocks.whole$MA30 & stocks.whole$PrevClose >= stocks.whole$PrevMA30]
+up.ma30 <- stocks.whole[stocks.whole$Close >= stocks.whole$MA30 & stocks.whole$PrevClose < stocks.whole$PrevMA30]
 down.up.ma30 <- rbind(down.ma30, up.ma30)
 down.up.ma30.day.diff <- diff(index(down.up.ma30))
 down.up.ma30.close.diff <- diff(down.up.ma30$Close)
